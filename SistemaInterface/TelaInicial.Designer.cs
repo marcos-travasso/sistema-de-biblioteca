bb384b69358791b8b71e8766050fa0313831568c
Marcos Travasso <marcos.travasso42@gmail.com>
2021-01-07 19:24:01 -0300

    Molde da tela inicial

--- SistemaInterface/TelaInicial.Designer.cs
+++ SistemaInterface/TelaInicial.Designer.cs
@@ -29,13 +29,223 @@ namespace SistemaInterface
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.imagemDaLogo = new System.Windows.Forms.PictureBox();
+            this.usuariosLabel = new System.Windows.Forms.Label();
+            this.livrosLabel = new System.Windows.Forms.Label();
+            this.usuariosCadastradosBotao = new System.Windows.Forms.Button();
+            this.usuariosEditarBotao = new System.Windows.Forms.Button();
+            this.livrosCadastrarBotao = new System.Windows.Forms.Button();
+            this.livrosCadastradosBotao = new System.Windows.Forms.Button();
+            this.livrosEditarBotao = new System.Windows.Forms.Button();
+            this.usuariosCadastrarBotao1 = new System.Windows.Forms.Button();
+            this.livrosPesquisarBotao = new System.Windows.Forms.Button();
+            this.novoEmprestimoBotao = new System.Windows.Forms.Button();
+            this.usuariosCadastrarBotao2 = new System.Windows.Forms.Button();
+            this.notificacoesListBox = new System.Windows.Forms.ListBox();
+            this.devolutivaBotao = new System.Windows.Forms.Button();
+            this.emprestimosBotao = new System.Windows.Forms.Button();
+            this.notificacoesLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imagemDaLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imagemDaLogo
+            // 
+            this.imagemDaLogo.Location = new System.Drawing.Point(1, 0);
+            this.imagemDaLogo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.imagemDaLogo.Name = "imagemDaLogo";
+            this.imagemDaLogo.Size = new System.Drawing.Size(243, 225);
+            this.imagemDaLogo.TabIndex = 1;
+            this.imagemDaLogo.TabStop = false;
+            // 
+            // usuariosLabel
+            // 
+            this.usuariosLabel.AutoSize = true;
+            this.usuariosLabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usuariosLabel.Location = new System.Drawing.Point(12, 246);
+            this.usuariosLabel.Name = "usuariosLabel";
+            this.usuariosLabel.Size = new System.Drawing.Size(142, 32);
+            this.usuariosLabel.TabIndex = 2;
+            this.usuariosLabel.Text = "→ Usuários";
+            // 
+            // livrosLabel
+            // 
+            this.livrosLabel.AutoSize = true;
+            this.livrosLabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.livrosLabel.Location = new System.Drawing.Point(12, 412);
+            this.livrosLabel.Name = "livrosLabel";
+            this.livrosLabel.Size = new System.Drawing.Size(111, 32);
+            this.livrosLabel.TabIndex = 3;
+            this.livrosLabel.Text = "→ Livros";
+            // 
+            // usuariosCadastradosBotao
+            // 
+            this.usuariosCadastradosBotao.Location = new System.Drawing.Point(54, 281);
+            this.usuariosCadastradosBotao.Name = "usuariosCadastradosBotao";
+            this.usuariosCadastradosBotao.Size = new System.Drawing.Size(154, 33);
+            this.usuariosCadastradosBotao.TabIndex = 4;
+            this.usuariosCadastradosBotao.Text = "Usuários cadastrados";
+            this.usuariosCadastradosBotao.UseVisualStyleBackColor = true;
+            // 
+            // usuariosEditarBotao
+            // 
+            this.usuariosEditarBotao.Location = new System.Drawing.Point(54, 359);
+            this.usuariosEditarBotao.Name = "usuariosEditarBotao";
+            this.usuariosEditarBotao.Size = new System.Drawing.Size(154, 33);
+            this.usuariosEditarBotao.TabIndex = 5;
+            this.usuariosEditarBotao.Text = "Editar usuários";
+            this.usuariosEditarBotao.UseVisualStyleBackColor = true;
+            // 
+            // livrosCadastrarBotao
+            // 
+            this.livrosCadastrarBotao.Location = new System.Drawing.Point(54, 486);
+            this.livrosCadastrarBotao.Name = "livrosCadastrarBotao";
+            this.livrosCadastrarBotao.Size = new System.Drawing.Size(154, 33);
+            this.livrosCadastrarBotao.TabIndex = 6;
+            this.livrosCadastrarBotao.Text = "Cadastrar novo livro";
+            this.livrosCadastrarBotao.UseVisualStyleBackColor = true;
+            // 
+            // livrosCadastradosBotao
+            // 
+            this.livrosCadastradosBotao.Location = new System.Drawing.Point(54, 447);
+            this.livrosCadastradosBotao.Name = "livrosCadastradosBotao";
+            this.livrosCadastradosBotao.Size = new System.Drawing.Size(154, 33);
+            this.livrosCadastradosBotao.TabIndex = 7;
+            this.livrosCadastradosBotao.Text = "Livros cadastrados";
+            this.livrosCadastradosBotao.UseVisualStyleBackColor = true;
+            // 
+            // livrosEditarBotao
+            // 
+            this.livrosEditarBotao.Location = new System.Drawing.Point(54, 525);
+            this.livrosEditarBotao.Name = "livrosEditarBotao";
+            this.livrosEditarBotao.Size = new System.Drawing.Size(154, 33);
+            this.livrosEditarBotao.TabIndex = 8;
+            this.livrosEditarBotao.Text = "Editar livros";
+            this.livrosEditarBotao.UseVisualStyleBackColor = true;
+            // 
+            // usuariosCadastrarBotao1
+            // 
+            this.usuariosCadastrarBotao1.Location = new System.Drawing.Point(54, 320);
+            this.usuariosCadastrarBotao1.Name = "usuariosCadastrarBotao1";
+            this.usuariosCadastrarBotao1.Size = new System.Drawing.Size(154, 33);
+            this.usuariosCadastrarBotao1.TabIndex = 9;
+            this.usuariosCadastrarBotao1.Text = "Cadastrar novo usuário";
+            this.usuariosCadastrarBotao1.UseVisualStyleBackColor = true;
+            // 
+            // livrosPesquisarBotao
+            // 
+            this.livrosPesquisarBotao.Location = new System.Drawing.Point(343, 177);
+            this.livrosPesquisarBotao.Name = "livrosPesquisarBotao";
+            this.livrosPesquisarBotao.Size = new System.Drawing.Size(267, 67);
+            this.livrosPesquisarBotao.TabIndex = 10;
+            this.livrosPesquisarBotao.Text = "Pesquisar Livros";
+            this.livrosPesquisarBotao.UseVisualStyleBackColor = true;
+            // 
+            // novoEmprestimoBotao
+            // 
+            this.novoEmprestimoBotao.Location = new System.Drawing.Point(343, 250);
+            this.novoEmprestimoBotao.Name = "novoEmprestimoBotao";
+            this.novoEmprestimoBotao.Size = new System.Drawing.Size(267, 67);
+            this.novoEmprestimoBotao.TabIndex = 11;
+            this.novoEmprestimoBotao.Text = "Novo empréstimo";
+            this.novoEmprestimoBotao.UseVisualStyleBackColor = true;
+            // 
+            // usuariosCadastrarBotao2
+            // 
+            this.usuariosCadastrarBotao2.Location = new System.Drawing.Point(343, 325);
+            this.usuariosCadastrarBotao2.Name = "usuariosCadastrarBotao2";
+            this.usuariosCadastrarBotao2.Size = new System.Drawing.Size(267, 67);
+            this.usuariosCadastrarBotao2.TabIndex = 12;
+            this.usuariosCadastrarBotao2.Text = "Novo usuário";
+            this.usuariosCadastrarBotao2.UseVisualStyleBackColor = true;
+            // 
+            // notificacoesListBox
+            // 
+            this.notificacoesListBox.FormattingEnabled = true;
+            this.notificacoesListBox.ItemHeight = 17;
+            this.notificacoesListBox.Location = new System.Drawing.Point(676, 58);
+            this.notificacoesListBox.Name = "notificacoesListBox";
+            this.notificacoesListBox.Size = new System.Drawing.Size(214, 259);
+            this.notificacoesListBox.TabIndex = 13;
+            // 
+            // devolutivaBotao
+            // 
+            this.devolutivaBotao.Location = new System.Drawing.Point(676, 342);
+            this.devolutivaBotao.Name = "devolutivaBotao";
+            this.devolutivaBotao.Size = new System.Drawing.Size(214, 50);
+            this.devolutivaBotao.TabIndex = 14;
+            this.devolutivaBotao.Text = "Devolutiva";
+            this.devolutivaBotao.UseVisualStyleBackColor = true;
+            // 
+            // emprestimosBotao
+            // 
+            this.emprestimosBotao.Location = new System.Drawing.Point(676, 398);
+            this.emprestimosBotao.Name = "emprestimosBotao";
+            this.emprestimosBotao.Size = new System.Drawing.Size(214, 50);
+            this.emprestimosBotao.TabIndex = 15;
+            this.emprestimosBotao.Text = "Livros emprestados";
+            this.emprestimosBotao.UseVisualStyleBackColor = true;
+            // 
+            // notificacoesLabel
+            // 
+            this.notificacoesLabel.AutoSize = true;
+            this.notificacoesLabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.notificacoesLabel.Location = new System.Drawing.Point(670, 23);
+            this.notificacoesLabel.Name = "notificacoesLabel";
+            this.notificacoesLabel.Size = new System.Drawing.Size(157, 32);
+            this.notificacoesLabel.TabIndex = 16;
+            this.notificacoesLabel.Text = "Notificações";
+            // 
+            // TelaInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(933, 588);
+            this.Controls.Add(this.notificacoesLabel);
+            this.Controls.Add(this.emprestimosBotao);
+            this.Controls.Add(this.devolutivaBotao);
+            this.Controls.Add(this.notificacoesListBox);
+            this.Controls.Add(this.usuariosCadastrarBotao2);
+            this.Controls.Add(this.novoEmprestimoBotao);
+            this.Controls.Add(this.livrosPesquisarBotao);
+            this.Controls.Add(this.usuariosCadastrarBotao1);
+            this.Controls.Add(this.livrosEditarBotao);
+            this.Controls.Add(this.livrosCadastradosBotao);
+            this.Controls.Add(this.livrosCadastrarBotao);
+            this.Controls.Add(this.usuariosEditarBotao);
+            this.Controls.Add(this.usuariosCadastradosBotao);
+            this.Controls.Add(this.livrosLabel);
+            this.Controls.Add(this.usuariosLabel);
+            this.Controls.Add(this.imagemDaLogo);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "TelaInicial";
+            this.Text = "Sistema Livrearia";
+            ((System.ComponentModel.ISupportInitialize)(this.imagemDaLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox imagemDaLogo;
+        private System.Windows.Forms.Label usuariosLabel;
+        private System.Windows.Forms.Label livrosLabel;
+        private System.Windows.Forms.Button usuariosCadastradosBotao;
+        private System.Windows.Forms.Button usuariosEditarBotao;
+        private System.Windows.Forms.Button livrosCadastrarBotao;
+        private System.Windows.Forms.Button livrosCadastradosBotao;
+        private System.Windows.Forms.Button livrosEditarBotao;
+        private System.Windows.Forms.Button usuariosCadastrarBotao1;
+        private System.Windows.Forms.Button livrosPesquisarBotao;
+        private System.Windows.Forms.Button novoEmprestimoBotao;
+        private System.Windows.Forms.Button usuariosCadastrarBotao2;
+        private System.Windows.Forms.ListBox notificacoesListBox;
+        private System.Windows.Forms.Button devolutivaBotao;
+        private System.Windows.Forms.Button emprestimosBotao;
+        private System.Windows.Forms.Label notificacoesLabel;
     }
 }
 
