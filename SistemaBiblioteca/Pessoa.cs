da36c69f71dee74ee12344e71696fe0ec8fb9122
Marcos Travasso <marcos.travasso42@gmail.com>
2021-01-08 18:48:20 -0300

    Mudança na classe para adequar ao banco de dados

--- SistemaBiblioteca/Pessoa.cs
+++ SistemaBiblioteca/Pessoa.cs
@@ -6,11 +6,18 @@ namespace SistemaBiblioteca
 {
     public class Pessoa
     {
+        public Pessoa() { }
+        public Pessoa(string Nome, string Genero, DateTime Nascimento)
+        {
+            this.Nome = Nome;
+            this.Genero = Genero;
+            this.Nascimento = Nascimento;
+        }
         public string Nome { get; set; }
 
         public string Genero { get; set; }
 
-        public string Nascimento { get; set; }
+        public DateTime Nascimento { get; set; }
 
         public int idPessoa { get; set; }
     }
