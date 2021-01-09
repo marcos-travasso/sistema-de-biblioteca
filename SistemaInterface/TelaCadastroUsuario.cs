34e338a2e60f3edee55e776a0080282ae31ea15e
Marcos Travasso <marcos.travasso42@gmail.com>
2021-01-09 13:37:24 -0300

    Tratamento de erros ao cadastrar usuário

--- SistemaInterface/TelaCadastroUsuario.cs
+++ SistemaInterface/TelaCadastroUsuario.cs
@@ -1,5 +1,6 @@
 ﻿using SistemaBiblioteca;
 using System;
+using System.Media;
 using System.Windows.Forms;
 
 namespace SistemaInterface
@@ -29,49 +30,68 @@ namespace SistemaInterface
         {
             if (generoTexto.Text != "" && nascimentoTexto.Text != "  /  /" && nomeTexto.Text != "")
             {
+                string genero;
+                DateTime nascimento;
+                Usuario novoUsuario = new Usuario();
+
+                if (generoTexto.Text == "Masculino") { novoUsuario.Genero = "M"; }
+                else if (generoTexto.Text == "Feminino") { novoUsuario.Genero = "F"; }
+                else { novoUsuario.Genero = "O"; }
+
+                try { nascimento = Convert.ToDateTime(nascimentoTexto.Text); }
+                catch { SystemSounds.Beep.Play(); MessageBox.Show("A data de nascimento está incorreta.", "Erro"); nascimentoTexto.Focus(); }
+
+                Endereco novoEndereco = new Endereco();
+
+                BancoDeDados banco = new BancoDeDados();
+
                 try
                 {
-                    string genero;
-                    DateTime nascimento = Convert.ToDateTime(nascimentoTexto.Text);
-
-                    if (generoTexto.Text == "Masculino") { genero = "M"; }
-                    else if (generoTexto.Text == "Feminino") { genero = "F"; }
-                    else { genero = "O"; }
-
-                    Usuario novoUsuario = new Usuario(nomeTexto.Text, genero, nascimento);
-                    Endereco novoEndereco = new Endereco();
-
-                    BancoDeDados banco = new BancoDeDados();
-
-
                     if (celularTexto.Text != "(  )      -") { novoUsuario.Celular = converterNumero(celularTexto.Text); } else { novoUsuario.Celular = ""; }
                     if (telefoneTexto.Text != "(  )     -") { novoUsuario.Telefone = converterNumero(telefoneTexto.Text); } else { novoUsuario.Telefone = ""; }
                     if (cpfTexto.Text != "   .   .   -") { novoUsuario.CPF = converterNumero(cpfTexto.Text); } else { novoUsuario.CPF = ""; }
                     if (emailTexto.Text != "") { novoUsuario.Email = emailTexto.Text; } else { novoUsuario.Email = ""; }
+                }
+                catch
+                {
+                    SystemSounds.Beep.Play();
+                    MessageBox.Show("Algum dado pessoal está inválido.", "Erro");
+                }
 
+                try
+                {
                     if (cepTexto.Text != "     -") { novoEndereco.CEP = converterNumero(cepTexto.Text); } else { novoEndereco.CEP = ""; }
                     if (cidadeTexto.Text != "") { novoEndereco.Cidade = cidadeTexto.Text; } else { novoEndereco.Cidade = ""; }
                     if (bairroTexto.Text != "") { novoEndereco.Bairro = bairroTexto.Text; } else { novoEndereco.Bairro = ""; }
                     if (ruaTexto.Text != "") { novoEndereco.Rua = ruaTexto.Text; } else { novoEndereco.Rua = ""; }
                     if (numeroTexto.Text != "") { novoEndereco.Numero = Convert.ToInt32(numeroTexto.Text); } else { novoEndereco.Numero = 0; }
                     if (complementoTexto.Text != "") { novoEndereco.Complemento = complementoTexto.Text; } else { novoEndereco.Complemento = ""; }
+                }
+                catch
+                {
+                    SystemSounds.Beep.Play();
+                    MessageBox.Show("Algum dado do endereço está inválido.", "Erro");
+                }
 
+                try
+                {
                     novoUsuario.Endereco = novoEndereco;
 
                     novoUsuario = banco.CriarUsuario(novoUsuario);
 
                     MessageBox.Show("Usuário cadastrado com sucesso!");
-
                 }
                 catch
                 {
-                    MessageBox.Show("Algum dado está inválido.");
+                    SystemSounds.Beep.Play();
+                    MessageBox.Show("Não foi possível cadastrar o usuário.", "Erro");
                 }
             }
             else
             {
-                //TODO dar focus no que falta
-                MessageBox.Show("Dados incorretos");
+                if (nomeTexto.Text == "") { SystemSounds.Beep.Play(); MessageBox.Show("É necessário um nome para criar usuário.", "Erro"); nomeTexto.Focus(); }
+                else if (nascimentoTexto.Text == "  /  /") { SystemSounds.Beep.Play(); MessageBox.Show("É necessário uma data de nascimento para criar usuário.", "Erro"); nascimentoTexto.Focus(); }
+                else if (generoTexto.Text == "") { SystemSounds.Beep.Play(); MessageBox.Show("É necessário um gênero para criar usuário.", "Erro"); generoTexto.Focus(); }
             }
         }
 
