da1b8c70e6d2072668a9270ef80376c944980bf8
Marcos Travasso <marcos.travasso42@gmail.com>
2021-01-11 17:33:06 -0300

    Classes para devolução e empréstimo

---
+++ SistemaBiblioteca/Devolucao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaBiblioteca
+{
+    public class Devolucao
+    {
+        public Devolucao() { }
+        public int idDevolucao { get; set; }
+        public Emprestimo emprestimo { get; set; }
+        public DateTime dataDeDevolucao { get; set; }
+    }
+}
---
+++ SistemaBiblioteca/Emprestimo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaBiblioteca
+{
+    public class Emprestimo
+    {
+        public Emprestimo() { }
+        public int idEmprestimo { get; set; }
+        public Livro livro { get; set; }
+        public Usuario usuario { get; set; }
+        public DateTime dataDoPedido { get; set; }
+        public List<Devolucao> devolucoes { get; set; }
+    }
+}
